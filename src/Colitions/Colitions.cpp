58546b5b726e042c05187024cb4f71e441a1a5a6
Dario <otrantodario@gmail.com>
2023-10-21 11:44:44 -0300

    Add Collitions Class

---
+++ src/Colitions/Colitions.cpp
@@ -0,0 +1,32 @@
+﻿#include "Colitions.h"
+#include <iostream>
+
+Colitions::Colitions()
+{
+    cout << "Colitions Created" << endl;
+}
+
+Colitions::~Colitions()
+{
+    cout << "Colitions Deleted" << endl;
+}
+
+bool Colitions::SquareSquareColition(Square a, Square b)
+{
+    return false;
+}
+
+bool Colitions::TriangleTriangleColition(Triangle a, Triangle b)
+{
+    return false; 
+}
+
+bool Colitions::SquareTriangleColition(Square a, Triangle b)
+{
+    return false;
+}
+
+
+
+
+
---
+++ src/Colitions/Colitions.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Shape/Square.h"
+#include "Shape/Triangle.h"
+
+class Colitions
+{
+public:
+    Colitions();
+    ~Colitions();
+    bool SquareSquareColition(Square a,Square b);
+    bool TriangleTriangleColition(Triangle a,Triangle b);
+    bool SquareTriangleColition(Square a,Triangle b);
+};
