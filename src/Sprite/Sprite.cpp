87aaa9fc6556e1e0211b2a2abb8691cebf77db3a
Rojoin <nachoarrastua@gmail.com>
2023-10-31 13:15:34 -0300

    Add animation to Frame

--- src/Sprite/Sprite.cpp
+++ src/Sprite/Sprite.cpp
@@ -1,6 +1,9 @@
 ﻿#include "Sprite.h"
 
-Sprite::Sprite(Renderer* renderer,Vec4 color, Vec3 position, Vec3 newScale,const char* imagePath): Entity2D(
+#include "Animation/Animation.h"
+#include "Animation/Animation.h"
+
+Sprite::Sprite(Renderer* renderer, Vec4 color, Vec3 position, Vec3 newScale, const char* imagePath): Entity2D(
     renderer, position, newScale)
 {
     vertexSize = 36;
@@ -9,33 +12,69 @@ Sprite::Sprite(Renderer* renderer,Vec4 color, Vec3 position, Vec3 newScale,const
 
     vertexPositions = new float[36]{
         // positions          // colors             // texture coords
-        0.5f,  0.5f, 0.0f,   1.0f,1.0f,1.0f,1.0f,   1.0f, 1.0f, // top right
-        0.5f, -0.5f, 0.0f,   1.0f,1.0f,1.0f,1.0f,   1.0f, 0.0f, // bottom right
-       -0.5f, -0.5f, 0.0f,   1.0f,1.0f,1.0f,1.0f,   0.0f, 0.0f, // bottom left
-       -0.5f,  0.5f, 0.0f,   1.0f,1.0f,1.0f,1.0f,   0.0f, 1.0f  // top left 
-   };
+        0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f // top left 
+    };
     //Crear clase Animation
     //En sprite tengo un puntero a animation, sino esta null
     //A animation le paso el vertexPositions para que toque los uvs
     //Crear animator? 
-    
+
     indexSize = 6;
     indices = new int[indexSize]{
         0, 1, 3,
         1, 2, 3
     };
     this->color = color;
-    renderer->CreateVecBuffer(vertexPositions, indices, vertexSize, indexSize, atribPosSize, VAO, VBO, EBO,atribColorSize,atribUVSize);
-    renderer->createTextureBinder(textureId, imagePath,this->width,this->height);
-    std::cout << "Sprite Created" <<endl;
+    renderer->CreateVecBuffer(vertexPositions, indices, vertexSize, indexSize, atribPosSize, VAO, VBO, EBO,
+                              atribColorSize, atribUVSize);
+    renderer->createTextureBinder(textureId, imagePath, this->width, this->height);
+    std::cout << "Sprite Created" << endl;
+    animation = Animation();
+}
+
+int Sprite::getWidth()
+{
+    return width;
+}
+
+int Sprite::getHeight()
+{
+    return height;
 }
 
 void Sprite::Draw()
 {
-   renderer->DrawSprite2D(VAO,indexSize,color,model,textureId);
+    renderer->DrawSprite2D(VAO, indexSize, color, model, textureId);
 }
 
 Sprite::~Sprite()
 {
     glDeleteBuffers(1, &textureId);
+}
+
+void Sprite::ChangeAnimation(Animation animation)
+{
+    this->animation = animation;
+}
+
+void Sprite::UpdateAnimation()
+{
+    if (animation.hasFrames())
+    {
+        int numberOfUVS = 4;
+        int strife = 7;
+        for (int i = 0; i < numberOfUVS; ++i)
+        {
+            ChangeUVCoord(strife * (i+1), animation.currentFrame.getUVCoord(i));
+        }
+    }
+}
+
+void Sprite::ChangeUVCoord(int strife, glm::vec2 UVS)
+{
+    vertexPositions[strife] = UVS.x;
+    vertexPositions[strife + 1] = UVS.y;
 }
--- src/Sprite/Sprite.h
+++ src/Sprite/Sprite.h
@@ -1,16 +1,21 @@
 ﻿#pragma once
+#include "Animation/Animation.h"
 #include "Shape/Shape.h"
 
-class EXPORT Sprite final : public Entity2D
+class EXPORT Sprite  : public Entity2D
 {
 private:
     int width;
     int height;
     int nrChannels;
+    Animation animation;
 public:
     Sprite(Renderer* renderer, Vec4 color, Vec3 position, Vec3 newScale, const char* imagePath);
     int getWidth();
     int getHeight();
     void Draw() override;
     ~Sprite() override;
+    void ChangeAnimation(Animation animation);
+    void UpdateAnimation();
+    void ChangeUVCoord(int strife, glm::vec2 UVS);
 };
