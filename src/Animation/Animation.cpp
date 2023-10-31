b7f7b09cfe81dd62553d2e16d5d4caecd8fcc1e1
Rojoin <nachoarrastua@gmail.com>
2023-10-31 13:15:00 -0300

    Add Animation class

---
+++ src/Animation/Animation.cpp
@@ -0,0 +1,17 @@
+﻿#include "Animation.h"
+
+
+
+Animation::Animation(int maxFrames, int frameTime, int spriteWidth, int spriteHeight, int frameWidth,
+    int frameHeight)
+{
+    
+}
+Animation::Animation()
+{
+    maxFramesInAnimation =0;
+}
+bool Animation::hasFrames()
+{
+    return maxFramesInAnimation >0;
+}
---
+++ src/Animation/Animation.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <vector>
+
+#include "Frame.h"
+#include "Sprite/Sprite.h"
+
+
+class final Animation
+{
+public:
+    Animation(int maxFrames, int frameTime, int spriteWidth, int spriteHeight, int frameWidth, int frameHeight);
+    Animation();
+    bool hasFrames();
+    Frame currentFrame;
+    
+private:
+    vector<Frame> framesInAnimation;
+    int spriteWidth;
+    int spriteHeight;
+    int frameWidth;
+    int frameHeight;
+    int currentFrameCounter;
+    int maxFramesInAnimation;
+    int frameTime;
+    int frameLengh;
+};
