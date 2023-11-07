e121d6d5a8107efccdd147f20920cddc3b914131
Rojoin <nachoarrastua@gmail.com>
2023-11-06 21:31:51 -0300

    Add Animation addFrames method

--- src/Animation/Animation.cpp
+++ src/Animation/Animation.cpp
@@ -29,3 +29,28 @@ bool Animation::hasFrames()
 {
     return maxFramesInAnimation > 0;
 }
+
+void Animation::addFrames()
+{
+    float spriteWidthF = spriteWidth;
+    float spriteHeightF = spriteHeight;
+    for (int i = 0; i < maxFramesInAnimation; ++i)
+    {
+        glm::vec2 topRight = glm::vec2(((i * frameWidth) + frameWidth) / spriteWidthF,
+                                       (frameHeight / spriteHeightF));
+        glm::vec2 botRight = glm::vec2(((i * frameWidth) + frameWidth) / spriteWidthF,
+                                       0);
+        glm::vec2 botLeft = glm::vec2((i * frameWidth) / spriteWidthF,
+                                      0);
+        glm::vec2 topLeft = glm::vec2((i * frameWidth) / spriteWidthF,
+                                      (frameHeight / spriteHeightF));
+
+        Frame* aux = new Frame(topRight, botRight, botLeft, topLeft);
+        totalFrames.push_front(aux);
+    }
+}
+
+void Animation::update()
+{
+    currentFrame = totalFrames.front();
+}
