063ce38163dff00e96b418476d2423054dff6b97
Rojoin <nachoarrastua@gmail.com>
2024-03-20 20:50:35 -0300

    Add Camera Class

---
+++ src/Camera/Camera.cpp
@@ -0,0 +1,63 @@
+﻿#include "Camera.h"
+
+Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch): Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED),
+                                                                          MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+{
+    Position = position;
+    WorldUp = up;
+    Yaw = yaw;
+    Pitch = pitch;
+    updateCameraVectors();
+}
+
+void Camera::checkKeywoardMovement(GLFWwindow* window)
+{
+    float currentTime = Time::getDeltaTime();
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        Position += MovementSpeed * Front * currentTime;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        Position -= MovementSpeed * Front * currentTime;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        Position -= glm::normalize(glm::cross(Front, Up)) * MovementSpeed * currentTime;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        Position += glm::normalize(glm::cross(Front, Up)) * MovementSpeed * currentTime;
+}
+
+void Camera::updateCameraVectors()
+{
+    // calculate the new Front vector
+    glm::vec3 front;
+    front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    front.y = sin(glm::radians(Pitch));
+    front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    Front = glm::normalize(front);
+    // also re-calculate the Right and Up vector
+    Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+    Up    = glm::normalize(glm::cross(Right, Front));
+}
+
+glm::mat4 Camera::getViewMatrix()
+{
+    return glm::lookAt(Position, Position + Front, Up);
+}
+
+void Camera::checkMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch)
+{
+    xoffset *= MouseSensitivity;
+    yoffset *= MouseSensitivity;
+
+    Yaw   += xoffset;
+    Pitch += yoffset;
+
+    // make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (constrainPitch)
+    {
+        if (Pitch > 89.0f)
+            Pitch = 89.0f;
+        if (Pitch < -89.0f)
+            Pitch = -89.0f;
+    }
+
+    // update Front, Right and Up Vectors using the updated Euler angles
+    updateCameraVectors();
+}
