c95a476cc0eff06561520d1b9c5e5b95d22e8fcb
Rojoin <nachoarrastua@gmail.com>
2024-03-20 22:08:07 -0300

    Update Game.cpp

--- Application/Game.cpp
+++ Application/Game.cpp
@@ -24,7 +24,7 @@ void Game::init()
     cartel = new Sprite(getRenderer(), {1, 1, 1, 1}, {150, 0, -400.0f}, {100, 100, 0},
                         playerPath,GL_NEAREST);
 
-    
+
     Animation animationPlayerRight = Animation(19, 205, 3, 0.5f, 830, 465, 40, 33);
     Animation animationPlayerIdle = Animation(390, 98, 3, 1.2f, 830, 465, 33, 40);
     Animation animationCartel = Animation(132, 94, 5, 0.5f, 830, 465, 49, 48);
@@ -34,17 +34,16 @@ void Game::init()
     Animator.insert_or_assign("cartel", animationCartel);
     cartel->ChangeAnimation(Animator["cartel"]);
     player1->ChangeAnimation(Animator["Right"]);
-
 }
 
 void Game::update()
 {
     Vec3 newPos = {player1->GetPosition().x, player1->GetPosition().y, player1->GetPosition().z};
     Vec3 scale = {player1->GetScale().x, player1->GetScale().y, player1->GetScale().z};
- 
+
 
     bool hasBeenPressed = false;
-    if (input->isKeyPressed(KeyKode::KEY_A))
+    if (input->isKeyPressed(KeyKode::KEY_LEFT))
     {
         newPos.x -= 1.0f * 2.0f;
         player1->SetPosition(newPos);
@@ -53,39 +52,35 @@ void Game::update()
         std::cout << player1->GetScale().z << endl;
         if (player1->GetScale().x > 0)
         {
-            player1->SetScale({player1->GetScale().x*-1, player1->GetScale().y, player1->GetScale().z });
+            player1->SetScale({player1->GetScale().x * -1, player1->GetScale().y, player1->GetScale().z});
         }
         hasBeenPressed = true;
-     
     }
-    if (input->isKeyPressed(KeyKode::KEY_W))
+    if (input->isKeyPressed(KeyKode::KEY_UP))
     {
-        newPos.y += 1.0f* 2.0f;
+        newPos.y += 1.0f * 2.0f;
         player1->SetPosition(newPos);
         player1->ChangeAnimation(Animator["Right"]);
         hasBeenPressed = true;
-      
     }
-    if (input->isKeyPressed(KeyKode::KEY_S))
+    if (input->isKeyPressed(KeyKode::KEY_DOWN))
     {
-        newPos.y -= 1.0f*  2.0f;
+        newPos.y -= 1.0f * 2.0f;
         player1->SetPosition(newPos);
         player1->ChangeAnimation(Animator["Right"]);
         hasBeenPressed = true;
-       
     }
-    if (input->isKeyPressed(KeyKode::KEY_D))
+    if (input->isKeyPressed(KeyKode::KEY_RIGHT))
     {
         newPos.x += 1.0f * 2;
         player1->SetPosition(newPos);
         player1->ChangeAnimation(Animator["Right"]);
         hasBeenPressed = true;
-        
+
         if (player1->GetScale().x < 0)
         {
-            player1->SetScale({player1->GetScale().x *-1, player1->GetScale().y, player1->GetScale().z});
+            player1->SetScale({player1->GetScale().x * -1, player1->GetScale().y, player1->GetScale().z});
         }
-
     }
     if (input->isKeyPressed(KeyKode::KEY_E))
     {
@@ -93,25 +88,23 @@ void Game::update()
         player1->SetPosition(newPos);
         player1->ChangeAnimation(Animator["Right"]);
         hasBeenPressed = true;
-        
+
         if (player1->GetScale().x < 0)
         {
-            player1->SetScale({player1->GetScale().x *-1, player1->GetScale().y, player1->GetScale().z});
+            player1->SetScale({player1->GetScale().x * -1, player1->GetScale().y, player1->GetScale().z});
         }
-      
     }
-if (input->isKeyPressed(KeyKode::KEY_Q))
+    if (input->isKeyPressed(KeyKode::KEY_Q))
     {
         newPos.z -= 1.0f * 2;
         player1->SetPosition(newPos);
         player1->ChangeAnimation(Animator["Right"]);
         hasBeenPressed = true;
-        
+
         if (player1->GetScale().x < 0)
         {
-            player1->SetScale({player1->GetScale().x *-1, player1->GetScale().y, player1->GetScale().z});
+            player1->SetScale({player1->GetScale().x * -1, player1->GetScale().y, player1->GetScale().z});
         }
-     
     }
 
     if (!hasBeenPressed)
@@ -130,10 +123,9 @@ void Game::exit()
     //Animator["Right"].deleteAnimation();
     //Animator["cartel"].deleteAnimation();
     //Animator["Idle"].deleteAnimation();
-    
+
     delete player1;
     delete obj1;
     delete obj2;
     delete cartel;
-    
 }
