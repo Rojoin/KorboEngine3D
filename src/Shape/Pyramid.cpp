22be94b3f97d00e600b8b45ba81a6627e16c3ab8
Rojoin <nachoarrastua@gmail.com>
2024-03-28 23:53:48 -0300

    Add Pyramid

---
+++ src/Shape/Pyramid.cpp
@@ -0,0 +1,24 @@
+﻿#include "Pyramid.h"
+
+Pyramid::Pyramid(Renderer* renderer, Vec3 position, Vec3 newScale) : Entity3D(renderer, position, newScale)
+{
+    vertexSize = 15;
+    vertexPositions = new float[vertexSize]{
+        0.5f, -0.5f, 0.5f, // bottom right
+        -0.5f, -0.5f, 0.5f, // bottom left
+        0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        0.0f, 0.5f, 0.0f
+    };
+    indexSize = 18;
+    indices = new int[indexSize]{
+        0, 1, 3,
+        1, 2, 3,
+        4, 0, 1,
+        4, 0, 2,
+        4, 2, 3,
+        4, 3, 1
+    };
+    this->color = {1, 1, 1, 1};
+    renderer->CreateVecBuffer(vertexPositions, indices, vertexSize, indexSize, atribPosSize, VAO, VBO, EBO);;
+}
---
+++ src/Shape/Pyramid.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include "Entity/Entity3D.h"
+
+class EXPORT Pyramid : public Entity3D
+{
+public:
+    Pyramid(Renderer* renderer,Vec3 position, Vec3 newScale);
+};
