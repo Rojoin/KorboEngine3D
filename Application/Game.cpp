ef05f6e50b6b47824a6f03fa306c13f18d225fe2
Rojoin <nachoarrastua@gmail.com>
2024-04-10 20:47:23 -0300

    Update Example to have dynamic Light

--- Application/Game.cpp
+++ Application/Game.cpp
@@ -26,8 +26,8 @@ void Game::init()
     cartel = new Sprite(getRenderer(), {1, 1, 1, 1}, {150, 0, -400.0f}, {100, 100, 0},
                         playerPath,GL_NEAREST);
     obj1 = new Sprite(getRenderer(), {1, 0, 0, 1}, {150, -100, -400.0f}, {5000, 5000, 0},
-                        "../res/images/parcialBackground.jpg",GL_NEAREST);
-    obj2 = new Pyramid(getRenderer(), {1, 1, 1}, {50, 50, 50},{1,0,0,1});
+                      "../res/images/parcialBackground.jpg",GL_NEAREST);
+    obj2 = new Cube(getRenderer(), {30, 1, 30}, {50, 50, 50}, {1, 0, 0, 1});
     obj1->SetRotationX(90);
     Animation animationPlayerRight = Animation(19, 205, 3, 0.5f, 830, 465, 40, 33);
     Animation animationPlayerIdle = Animation(390, 98, 3, 1.2f, 830, 465, 33, 40);
@@ -37,43 +37,44 @@ void Game::init()
     Animator.insert_or_assign("Idle", animationPlayerIdle);
     Animator.insert_or_assign("cartel", animationCartel);
     cartel->ChangeAnimation(Animator["cartel"]);
-    player1->ChangeAnimation(Animator["Right"]);
+    player1->ChangeAnimation(Animator["Idle"]);
 }
 
 void Game::update()
 {
     Vec3 newPos = {player1->GetPosition().x, player1->GetPosition().y, player1->GetPosition().z};
-    Vec3 scale = {player1->GetScale().x, player1->GetScale().y, player1->GetScale().z}; 
+    Vec3 scale = {player1->GetScale().x, player1->GetScale().y, player1->GetScale().z};
 
 
     bool hasBeenPressed = false;
 
     float MovementSpeed = 100.5f;
-    
-    if ((input->isKeyPressed(KeyKode::KEY_W)))
-    {
-        newPos.z -= MovementSpeed * getDeltaTime();
-        player1->SetPosition(newPos);
-    }
 
-    if ((input->isKeyPressed(KeyKode::KEY_S)))
-    {
-        newPos.z += MovementSpeed * getDeltaTime();
-        player1->SetPosition(newPos);
-    }
-    if ((input->isKeyPressed(KeyKode::KEY_A)))
-    {
-        newPos.x -= MovementSpeed * getDeltaTime();
-        player1->SetPosition(newPos);
-        
-    }
-    if ((input->isKeyPressed(KeyKode::KEY_D)))
-    {
-        newPos.x += MovementSpeed * getDeltaTime();
-        player1->SetPosition(newPos);
-    }
-    
-    camera->changeCameraObjetive(glm::vec3(newPos.x,newPos.y,newPos.z),glm::vec3(player1->GetRotation().x,player1->GetRotation().y,player1->GetRotation().z));
+#pragma region Movement with WASD
+    // if ((input->isKeyPressed(KeyKode::KEY_W)))
+    // {
+    //     newPos.z -= MovementSpeed * getDeltaTime();
+    //     hasBeenPressed = true;
+    //     player1->ChangeAnimation(Animator["Idle"]);
+    // }
+    // if ((input->isKeyPressed(KeyKode::KEY_S)))
+    // {
+    //     newPos.z += MovementSpeed * getDeltaTime();
+    //     hasBeenPressed = true;
+    // }
+    // if ((input->isKeyPressed(KeyKode::KEY_A)))
+    // {
+    //     newPos.x -= MovementSpeed * getDeltaTime();
+    //
+    //     hasBeenPressed = true;
+    // }
+    // if ((input->isKeyPressed(KeyKode::KEY_D)))
+    // {
+    //     newPos.x += MovementSpeed * getDeltaTime();
+    //     hasBeenPressed = true;
+    // }
+#pragma endregion
+#pragma region Movement with Arrows
     if (input->isKeyPressed(KeyKode::KEY_LEFT))
     {
         newPos.x -= 1.0f * 2.0f;
@@ -89,14 +90,14 @@ void Game::update()
     }
     if (input->isKeyPressed(KeyKode::KEY_UP))
     {
-        newPos.y += 1.0f * 2.0f;
+        newPos.z -= 1.0f * 2.0f;
         player1->SetPosition(newPos);
         player1->ChangeAnimation(Animator["Right"]);
         hasBeenPressed = true;
     }
     if (input->isKeyPressed(KeyKode::KEY_DOWN))
     {
-        newPos.y -= 1.0f * 2.0f;
+        newPos.z += 1.0f * 2.0f;
         player1->SetPosition(newPos);
         player1->ChangeAnimation(Animator["Right"]);
         hasBeenPressed = true;
@@ -115,26 +116,22 @@ void Game::update()
     }
     if (input->isKeyPressed(KeyKode::KEY_E))
     {
-    
         player1->ChangeAnimation(Animator["Right"]);
         hasBeenPressed = true;
-
         player1->SetRotationY(1.0f);
     }
     if (input->isKeyPressed(KeyKode::KEY_Q))
     {
-
         player1->ChangeAnimation(Animator["Right"]);
         hasBeenPressed = true;
 
         player1->SetRotationY(-1.0f);
     }
+#pragma endregion
+    camera->changeCameraObjetive(glm::vec3(newPos.x, newPos.y, newPos.z),glm::vec3(player1->GetRotation().x, player1->GetRotation().y,player1->GetRotation().z));
 
-    if (!hasBeenPressed)
-    {
-        //player1->ChangeAnimation(Animator["Idle"]);
-    }
-
+    player1->SetPosition(newPos);
+    setLightPos(newPos);
     player1->UpdateAnimation();
     cartel->UpdateAnimation();
     cartel->Draw();
