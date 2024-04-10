27e4ff96f67740cb7fc8b240b9bec55765028d44
Rojoin <nachoarrastua@gmail.com>
2024-04-10 19:05:30 -0300

    Add new Vertex with Normals for Cube.cpp

--- src/Shape/Cube.cpp
+++ src/Shape/Cube.cpp
@@ -2,64 +2,167 @@
 
 Cube::Cube(Renderer* renderer, Vec3 position, Vec3 newScale) : Entity3D(renderer, position, newScale)
 {
-    vertexSize = 24;
+    vertexSize = 216;
+
     vertexPositions = new float[vertexSize]{
-        0.5f, 0.5f, 0.5f, // top right
-        0.5f, -0.5f, 0.5f, // bottom right
-        -0.5f, -0.5f, 0.5f, // bottom left
-        -0.5f, 0.5f, 0.5f, // top Left
-        0.5f, 0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, 0.5f, -0.5f
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, -0.5f,   0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, -0.5f,   0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, -1.0f,
+        0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -1.0f,
+        0.5f, 0.5f, 0.5f,   0.0f, 0.0f, -1.0f,
+        0.5f, 0.5f, 0.5f,   0.0f, 0.0f, -1.0f,
+        -0.5f, 0.5f, 0.5f,  0.0f, 0.0f, -1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, -1.0f,
+
+        -0.5f, 0.5f, 0.5f,   1.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f,  1.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f,   1.0f, 0.0f, 0.0f,
+
+        0.5f, 0.5f, 0.5f,   -1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, -0.5f,  -1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.5f,  -1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.5f,   -1.0f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 0.0f,  1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 0.0f,   1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 0.0f,   1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+
+        -0.5f, 0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 0.0f,  -1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 0.0f,   -1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 0.0f,   -1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f,  -1.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, -1.0f, 0.0f
     };
+
     indexSize = 36;
     indices = new int[indexSize]{
-        0, 1, 3,
-        1, 2, 3,
-        4, 5, 7,
-        5, 6, 7,
-        0, 3, 4,
-        3, 4, 7,
-        1, 2, 5,
-        2, 6, 5,
-        1, 0, 5,
-        0, 4, 5,
-        3, 2, 6,
-        3, 7, 6
+        0, 1, 2,
+        3, 4, 5,
+        6, 7, 8,
+        9, 10, 11,
+        12, 13, 14,
+        15, 16, 17,
+        18, 19, 20,
+        21, 22, 23,
+        24, 25, 26,
+        27, 28, 29,
+        30, 31, 32,
+        33, 34, 35
     };
-    this->color = {1,1,1,1};
-    renderer->CreateVecBuffer(vertexPositions, indices, vertexSize, indexSize, atribPosSize, VAO, VBO, EBO);;
+    int atribNormalSize = 3;
+    this->color = {1, 1, 1, 1};
+    renderer->createVecBufferWithNormals(vertexPositions, indices, vertexSize, indexSize, atribPosSize, atribNormalSize,
+                                         VAO, VBO,
+                                         EBO);
 }
+
 Cube::Cube(Renderer* renderer, Vec3 position, Vec3 newScale, Vec4 color) : Entity3D(renderer, position, newScale)
 {
-    vertexSize = 24;
+    vertexSize = 48;
+
+
+    //vertexPositions = new float[vertexSize]{
+    //    0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, // top right
+    //    0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, // bottom right
+    //    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, // bottom left
+    //    -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, // top Left
+    //    0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+    //    0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+    //    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+    //    -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+    //};
+    //indexSize = 36;
+    //indices = new int[indexSize]{
+    //    0, 1, 3,
+    //    1, 2, 3,
+    //    4, 5, 7,
+    //    5, 6, 7,
+    //    0, 3, 4,
+    //    3, 4, 7,
+    //    1, 2, 5,
+    //    2, 6, 5,
+    //    1, 0, 5,
+    //    0, 4, 5,
+    //    3, 2, 6,
+    //    3, 7, 6
+    //};
+
+    vertexSize = 216;
+
     vertexPositions = new float[vertexSize]{
-        0.5f, 0.5f, 0.5f, // top right
-        0.5f, -0.5f, 0.5f, // bottom right
-        -0.5f, -0.5f, 0.5f, // bottom left
-        -0.5f, 0.5f, 0.5f, // top Left
-        0.5f, 0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, 0.5f, -0.5f
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, -0.5f,   0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, -0.5f,   0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, -1.0f,
+        0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -1.0f,
+        0.5f, 0.5f, 0.5f,   0.0f, 0.0f, -1.0f,
+        0.5f, 0.5f, 0.5f,   0.0f, 0.0f, -1.0f,
+        -0.5f, 0.5f, 0.5f,  0.0f, 0.0f, -1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, -1.0f,
+
+        -0.5f, 0.5f, 0.5f,   1.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f,  1.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f,   1.0f, 0.0f, 0.0f,
+
+        0.5f, 0.5f, 0.5f,   -1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, -0.5f,  -1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.5f,  -1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.5f,   -1.0f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 0.0f,  1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 0.0f,   1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 0.0f,   1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+
+        -0.5f, 0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 0.0f,  -1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 0.0f,   -1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 0.0f,   -1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f,  -1.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, -1.0f, 0.0f
     };
     indexSize = 36;
     indices = new int[indexSize]{
-        0, 1, 3,
-        1, 2, 3,
-        4, 5, 7,
-        5, 6, 7,
-        0, 3, 4,
-        3, 4, 7,
-        1, 2, 5,
-        2, 6, 5,
-        1, 0, 5,
-        0, 4, 5,
-        3, 2, 6,
-        3, 7, 6
+        0, 1, 2,
+        3, 4, 5,
+        6, 7, 8,
+        9, 10, 11,
+        12, 13, 14,
+        15, 16, 17,
+        18, 19, 20,
+        21, 22, 23,
+        24, 25, 26,
+        27, 28, 29,
+        30, 31, 32,
+        33, 34, 35
     };
+    int atribNormalSize = 3;
     this->color = {color.x, color.y, color.z, color.w};
-    renderer->CreateVecBuffer(vertexPositions, indices, vertexSize, indexSize, atribPosSize, VAO, VBO, EBO);;
+    renderer->createVecBufferWithNormals(vertexPositions, indices, vertexSize, atribNormalSize, atribPosSize, indexSize,VAO, VBO,EBO);
 }
 
