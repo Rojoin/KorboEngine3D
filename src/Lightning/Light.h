f645bbd9e33a775a56752b91c07b051b9a88967c
Rojoin <nachoarrastua@gmail.com>
2024-04-17 20:03:47 -0300

    Add Light Struct

---
+++ src/Lightning/Light.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <glm/vec3.hpp>
+
+struct Light
+{
+    glm::vec3 position;
+    
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+
+    Light()
+    {
+        ambient = glm::vec3( 0.2f, 0.2f, 0.2f);
+        diffuse = glm::vec3(  0.5f, 0.5f, 0.5f);
+        specular = glm::vec3(1.0f, 1.0f, 1.0f);
+    }
+};
