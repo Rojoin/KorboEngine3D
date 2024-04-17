5c05a5a756bf9ca3d8519aea0dc2abc2eaac950b
Rojoin <nachoarrastua@gmail.com>
2024-04-17 20:18:52 -0300

    Add Light Constructors

--- src/Lightning/Light.h
+++ src/Lightning/Light.h
@@ -4,15 +4,30 @@
 struct Light
 {
     glm::vec3 position;
-    
+
     glm::vec3 ambient;
     glm::vec3 diffuse;
     glm::vec3 specular;
 
     Light()
     {
-        ambient = glm::vec3( 0.2f, 0.2f, 0.2f);
-        diffuse = glm::vec3(  0.5f, 0.5f, 0.5f);
+        ambient = glm::vec3(0.2f, 0.2f, 0.2f);
+        diffuse = glm::vec3(0.5f, 0.5f, 0.5f);
         specular = glm::vec3(1.0f, 1.0f, 1.0f);
+    }
+
+    Light(glm::vec3 amb, glm::vec3 dif, glm::vec3 spec)
+    {
+        ambient = amb;
+        diffuse = dif;
+        specular = spec;
+    }
+
+    Light(glm::vec3 pos, glm::vec3 amb, glm::vec3 dif, glm::vec3 spec)
+    {
+        position = pos;
+        ambient = amb;
+        diffuse = dif;
+        specular = spec;
     }
 };
