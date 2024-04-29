2154a38f9b4226f2b0df5b69ababa1f2568b3d52
Rojoin <nachoarrastua@gmail.com>
2024-04-29 04:17:43 -0300

    Add Directional and SpotLights

--- src/Lightning/DirectionLight.h
+++ src/Lightning/DirectionLight.h
@@ -1,31 +1,31 @@
 ﻿#pragma once
 #include <glm/vec3.hpp>
 
-struct Light
+struct DirectionLight
 {
-    glm::vec3 position;
+    glm::vec3 direction;
 
     glm::vec3 ambient;
     glm::vec3 diffuse;
     glm::vec3 specular;
 
-    Light()
+    DirectionLight()
     {
         ambient = glm::vec3(0.2f, 0.2f, 0.2f);
         diffuse = glm::vec3(0.5f, 0.5f, 0.5f);
         specular = glm::vec3(1.0f, 1.0f, 1.0f);
     }
 
-    Light(glm::vec3 amb, glm::vec3 dif, glm::vec3 spec)
+    DirectionLight(glm::vec3 amb, glm::vec3 dif, glm::vec3 spec)
     {
         ambient = amb;
         diffuse = dif;
         specular = spec;
     }
 
-    Light(glm::vec3 pos, glm::vec3 amb, glm::vec3 dif, glm::vec3 spec)
+    DirectionLight(glm::vec3 dir, glm::vec3 amb, glm::vec3 dif, glm::vec3 spec)
     {
-        position = pos;
+        direction = dir;
         ambient = amb;
         diffuse = dif;
         specular = spec;
---
+++ src/Lightning/SpotLight.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include <glm/trigonometric.hpp>
+#include <glm/vec3.hpp>
+
+
+struct SpotLight
+{
+public:
+    glm::vec3 direction;
+    glm::vec3 position;
+
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    float constant;
+    float linear;
+    float quadratic;
+    float cutOff;
+    float outerCutOff;
+
+    SpotLight(glm::vec3 dir, glm::vec3 pos, glm::vec3 amb, glm::vec3 diff, glm::vec3 spec, float con, float lin,
+              float quadra, float cut, float outerCut)
+    {
+        direction = dir;
+        position = pos;
+        ambient = amb;
+        diffuse = diff;
+        specular = spec;
+        constant = con;
+        linear = lin;
+        quadratic = quadra;
+        cutOff = cut;
+        outerCutOff = outerCut;
+    }
+
+    SpotLight(): direction(glm::vec3(0)), position(glm::vec3(0)), ambient(glm::vec3(0.0f, 0.0f, 0.0f)),
+                 diffuse(glm::vec3(1.0f, 1.0f, 1.0f)), specular(glm::vec3(1.0f, 1.0f, 1.0f)), constant(1.0f),
+                 linear(0.09f), quadratic(0.032f),
+                 cutOff(glm::cos(glm::radians(12.5f))),
+                 outerCutOff(glm::cos(glm::radians(15.0f)))
+    {
+    }
+
+    void setDirAndPos(const glm::vec3& dir, const glm::vec3& pos)
+    {
+        direction = dir;
+        position = pos;
+    }
+};
